cebcbe779a59e364902338cb63906dea33f1510e
Vitalii Voitovich <voitovich.ukrtechinfo@gmail.com>
2022-07-08 14:52:03 +0300

    bot token returns

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ app.action("verify_cancel_button", async ({ ack, body, client }) => {
 app.action("moderator_action_settings", async ({ ack, client, body, action }) => {
     await ack();
 
-    const list = await client.conversations.list({ types: "public_channel,private_channel", token: process.env.SLACK_APP_TOKEN });
+    const list = await client.conversations.list({ types: "public_channel,private_channel", token: process.env.SLACK_BOT_TOKEN });
 
     const channelsAsOptions = list.channels.map(ch => ({
         "text": {
